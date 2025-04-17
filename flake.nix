960931a9c888efc96d62d7c985a404cb8ba30904
RedEtherbloom <etherbloom+github@mailbox.org>
2025-04-17 18:35:25 +0200

    feat: add jupyter runtime

--- flake.nix
+++ flake.nix
@@ -1,30 +1,51 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.poetry2nix.url = "github:nix-community/poetry2nix";
+  description = "Various AI and ML experiments involving OpenAI and LangChain";
+  # Default substituters provided by tweag-jupyter are disabled for now
 
-  outputs = { self, nixpkgs, poetry2nix }:
-    let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
-    in
-    {
-      packages = forAllSystems (system: let
-        inherit (poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; }) mkPoetryApplication;
-      in {
-        default = mkPoetryApplication { projectDir = self; };
-      });
+  inputs = {
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    # See: https://github.com/tweag/jupyenv/issues/571
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    jupyenv.url = "github:tweag/jupyenv";
+  };
 
-      devShells = forAllSystems (system: let
-        inherit (poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; }) mkPoetryEnv;
+  outputs = {
+    self,
+    flake-utils,
+    nixpkgs,
+    jupyenv,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachSystem
+    [
+      flake-utils.lib.system.x86_64-linux
+    ]
+    (
+      system: let
+        pkgs = import nixpkgs {
+          allowUnfree = true;
+          inherit system;
+        };
+        jupyterlab = jupyenv.lib.${system}.mkJupyterlabNew ({...}: {
+          inherit nixpkgs;
+          imports = [(import ./kernels.nix)];
+        });
       in {
-        default = pkgs.${system}.mkShellNoCC {
-          packages = with pkgs.${system}; [
-            # Broken due to jiter having a weird cargo build
-            # (mkPoetryEnv { projectDir = self; })
+        packages = {inherit jupyterlab;};
+        packages.default = jupyterlab;
+        apps.default.program = "${jupyterlab}/bin/jupyter-lab";
+        apps.default.type = "app";
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
             poetry
+            jupyterlab
           ];
         };
-      });
-    };
+      }
+    );
 }
