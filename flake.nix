a4aa1e97794df4b934979ade4db3afbf1d18554e
RedEtherbloom <etherbloom+github@mailbox.org>
2025-04-25 00:20:46 +0200

    fix: swapout jupyenv for stock poetry
    Various cargo and dependency issues arose due to jupyenv env
    implementation. Additionally the project looks a bit stale from cursory
    glances.

--- flake.nix
+++ flake.nix
@@ -1,23 +1,15 @@
 {
   description = "Various AI and ML experiments involving OpenAI and LangChain";
-  # Default substituters provided by tweag-jupyter are disabled for now
 
   inputs = {
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    # See: https://github.com/tweag/jupyenv/issues/571
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    jupyenv.url = "github:tweag/jupyenv";
   };
 
   outputs = {
     self,
     flake-utils,
     nixpkgs,
-    jupyenv,
     ...
   } @ inputs:
     flake-utils.lib.eachSystem
@@ -30,21 +22,15 @@
           allowUnfree = true;
           inherit system;
         };
-        jupyterlab = jupyenv.lib.${system}.mkJupyterlabNew ({...}: {
-          inherit nixpkgs;
-          imports = [(import ./kernels.nix)];
-        });
       in {
-        packages = {inherit jupyterlab;};
-        packages.default = jupyterlab;
-        apps.default.program = "${jupyterlab}/bin/jupyter-lab";
-        apps.default.type = "app";
-
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             poetry
-            jupyterlab
           ];
+
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
+            stdenv.cc.cc.lib
+          ]);
         };
       }
     );
